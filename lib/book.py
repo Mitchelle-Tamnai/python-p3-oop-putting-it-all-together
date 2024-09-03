8066ea93df4bba990cac27193ac592506ac0eff5
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-09-03 13:12:03 +0300

    Passed tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        """Gets the number of pages in the book."""
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        """Turns the page in the book."""
+        self._page_count -= 1
+        print("Flipping the page...wow, you read fast!")
     pass
     
         
